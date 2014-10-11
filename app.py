f5f394a6e9a848abf8e1ec1d5a0ccf8624ccc1e3
AdamDeHovitz <adam.dehovitz@gmail.com>
2014-10-11 11:32:17 -0400

    Made the website better
    Random individual now answers the question

--- app.py
+++ app.py
@@ -8,21 +8,71 @@ app = Flask(__name__)
 @app.route("/", methods = ["POST", "GET"])
 def start():
     question = None;
+    answer = None;
     if request.method == 'POST':
         question = request.form["question"]
         #Operate on question using google stuff here
+    if (question != None):    
+        l = picture(question);
+        return render_template("index.html", answer = question, name = l[0], image = l[1], adjective = l[2]  )
+
+
+
+
+    return render_template("index.html")
+
+def imgReader():
+  filez = open('./Static/namesAndImages.txt', 'r')
+  Dict=filez.read()
+  Dict=Dict.split('\n')
+  Dict=Dict[:len(Dict)-1]
+  filez.close()
+  return Dict
+def titleReader ():
+  filezd = open('./Static/Titles.txt', 'r')
+  titles=filezd.read()
+  titles=titles.split('\n')
+  titles=titles[:len(titles)-2]
+  filezd.close()
+  return titles
+
+
+def picture(answer):
+   
+    l = []
+    
+     
+
+
+    
+
+
+    imgRepo = imgReader()
+    Image=imgRepo[random.randint(0, len(imgRepo)-1)]
+    Image=Image.split(',')
+    Name = Image[0]
+    image = Image[1]
+    l.append(Name);
+    l.append(image);
+    titles=titleReader()
+
+    l.append(titles[random.randint(0, len(titles)-1)])
         
 
+    #get_it.close()
 
 
 
-    return render_template("index.html", answer = question)
+    return l
+
+
 
 
 if __name__=="__main__":
     app.debug=True
     app.run()
 
+    
 
 
 #f = open('dates', 'r') #data will be the stuff from web pages
